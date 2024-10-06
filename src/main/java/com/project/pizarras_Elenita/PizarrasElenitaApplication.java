29c24ec4bc252880a09962baa82d9fde6b33f2ab
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-10-06 00:24:11 -0500

    First commit

---
+++ src/main/java/com/project/pizarras_Elenita/PizarrasElenitaApplication.java
@@ -0,0 +1,13 @@
+package com.project.pizarras_Elenita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizarrasElenitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizarrasElenitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pizarras_Elenita/PizarrasElenitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pizarras_Elenita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizarrasElenitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
