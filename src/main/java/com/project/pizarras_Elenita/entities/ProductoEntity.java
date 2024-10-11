c739bb0647f904f68966930fb57d79a81863db01
LadySC <seminarioeliana5@gmail.com>
2024-10-10 23:48:33 -0500

    Se agrega la entidad Producto.

---
+++ src/main/java/com/project/pizarras_Elenita/entities/ProductoEntity.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.pizarras_Elenita.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="producto")
+public class ProductoEntity {
+
+    @Id
+    @Column(name = "producto_id", length = 6, nullable = false)
+    private String productoId;
+
+    @Column(name = "nombre_producto", length = 100)
+    private String nombreProducto;
+
+    @Column(name = "descripcion", length = 255)
+    private String descripcion;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+}
