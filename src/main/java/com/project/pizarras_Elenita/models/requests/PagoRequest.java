0072b807a448b59a50fda2a07fd8b42760a2dfed
AlealarcViva <U22200142@utp.edu.pe>
2024-10-16 21:47:58 -0500

    Se sube DeliveryRequest y PagoRequest

---
+++ src/main/java/com/project/pizarras_Elenita/models/requests/PagoRequest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.pizarras_Elenita.models.requests;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagoRequest {
+
+    private String pedidoId;
+    private Date fechaPago;
+    private BigDecimal monto;
+    private String metodoPago;
+}
