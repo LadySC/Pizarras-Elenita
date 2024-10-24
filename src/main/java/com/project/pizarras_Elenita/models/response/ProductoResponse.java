c95b12846204665e7531d09bbb4c1670d8d6054b
LadySC <seminarioeliana5@gmail.com>
2024-10-23 21:30:48 -0500

    Se agrega el ProductoResponse

--- src/main/java/com/project/pizarras_Elenita/models/response/ProductoResponse.java
+++ src/main/java/com/project/pizarras_Elenita/models/response/ProductoResponse.java
@@ -4,10 +4,16 @@
  */
 package com.project.pizarras_Elenita.models.response;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Ryzen
  */
 public class ProductoResponse {
-    
+    private String productName;
+    private String Id;
+    private String description;
+    private BigDecimal price;
+    private long stock;
 }
